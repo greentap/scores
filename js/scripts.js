bcd908cfd4498219f555d9958702cd2b69b10bab
tash-had <nanylagoon@gmail.com>
2015-04-03 05:22:32 -0400

    Added Mobile Layout
    Didn't have time to make a full-fledged mobile design but did however
    set separate settings for mobile views.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,49 @@
 
+var mCheck = false; 
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		mCheck = true; 
+		document.getElementById("dButton").style.height = "300px"; 
+		document.getElementById("dButton").style.width = "100%"; 
+		document.getElementById("contactNav").innerHTML = "";
+		document.getElementById("dButton").style.fontSize = "60px";
+		document.getElementById("titleText").style.fontSize= "130px"; 
+		document.getElementById("mainCaption").style.fontSize= "45px"; 
+		document.getElementById("mainCaption").style.textAlign= "center"; 
+		}
+
+
 function hover(id){
-	document.getElementById(id).style.color="black";
+	if(mCheck == false){
+		document.getElementById(id).style.color="black";
+	}
 }
 
 function leave(id){
-	document.getElementById(id).style.color="white";
-
+	if(mCheck == false){
+		document.getElementById(id).style.color="white";
+	}
 }
 
 function hoverText(id){
-	document.getElementById(id).style.color="#00c853"; 
-	document.getElementById("contactNav").style.color="#00c853"; 
-	document.getElementById("dButton").style.height = "100px"; 
-	document.getElementById("dButton").style.width = "600px"; 
-	document.getElementById("dButton").innerHTML = "just download it already";
-	document.body.style.background = ("black"); 
-
+	if(mCheck == false){
+		document.getElementById(id).style.color="#00c853"; 
+		document.getElementById("contactNav").style.color="#00c853"; 
+		document.getElementById("dButton").style.height = "100px"; 
+		document.getElementById("dButton").style.width = "600px"; 
+		document.getElementById("dButton").innerHTML = "just download it already";
+		document.body.style.background = ("black"); 
+		}
 
 }
 
 function leaveText(id){
-	document.getElementById(id).style.color="white"; 
-	document.getElementById("contactNav").style.color="white"; 
-	document.getElementById("dButton").style.height = "50px"; 
-	document.getElementById("dButton").style.width = "300px"; 
-	document.getElementById("dButton").innerHTML = "ANDROID DOWNLOAD";
-	document.body.style.background = ("#00c853"); 
-
-
+	if(mCheck == false){
+		document.getElementById(id).style.color="white"; 
+		document.getElementById("contactNav").style.color="white"; 
+		document.getElementById("dButton").style.height = "50px"; 
+		document.getElementById("dButton").style.width = "300px"; 
+		document.getElementById("dButton").innerHTML = "ANDROID DOWNLOAD";
+		document.body.style.background = ("#00c853"); 
+		}
 }
